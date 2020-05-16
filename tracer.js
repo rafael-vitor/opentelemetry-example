fcebaa6ee0dec15b482e48197289f90eecf0fec8
rafael-vitor <rafael.vitor@jusbrasil.com.br>
2020-05-16 20:31:16 -0300

    Cleanup tracer code

--- tracer.js
+++ tracer.js
@@ -1,20 +1,11 @@
-const opentelemetry = require('@opentelemetry/api');
 const { NodeTracerProvider } = require('@opentelemetry/node');
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { SimpleSpanProcessor } = require('@opentelemetry/tracing');
 
-// const provider = new NodeTracerProvider({ logLevel: 3, });
 const createTracer = (serviceName) => {
   const provider = new NodeTracerProvider();
-
-  // const serviceName = 'xesk-redis';
-
-  exporter = new JaegerExporter({
-    serviceName,
-  });
-
+  exporter = new JaegerExporter({ serviceName });
   provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-
   provider.register();
 }
 
